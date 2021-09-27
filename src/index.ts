a2d286def25d40714cf11a7d1610819754227de0
akm <akm2000@gmail.com>
2021-09-28 02:00:54 +0900

    :sparkles: 引数のgoモジュールのパスとバージョンを元にpkg.go.dev と github.com からライセンスを取得

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,82 @@
-console.log('Try npm run lint/fix!');
+import {chromium} from 'playwright';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+let input_string = '';
 
-const trailing = 'Semicolon';
+process.stdin.on('data', chunk => {
+  input_string += chunk;
+});
 
-const why = 'am I tabbed?';
+process.stdin.on('end', () => {
+  const linesText = input_string;
+  // console.log(linesText);
 
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
-}
-// TODO: more examples
+  const version0 = /^v0\.0\.0-/;
+
+  (async () => {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+
+    const pkgGoDevSelector =
+      '[data-test-id=UnitHeader-licenses] [data-test-id=UnitHeader-license]';
+    const githubSelector = 'h3:has-text("License") + .mt-3 a';
+
+    try {
+      const lines = linesText.split('\n');
+      for (const line of lines) {
+        if (!line.trim()) continue;
+
+        const d = JSON.parse(line);
+        const path = d['Path'] as string;
+        const version = d['Version'] as string;
+
+        const urls: string[] = [];
+        const urlToSelector: {[key: string]: string} = {};
+
+        const add = (url: string, selector: string) => {
+          urls.push(url);
+          urlToSelector[url] = selector;
+        };
+
+        if (!version.match(version0))
+          add(`https://pkg.go.dev/${path}@${version}`, pkgGoDevSelector);
+        add(`https://pkg.go.dev/${path}`, pkgGoDevSelector);
+
+        if (path.match(/^github\.com\//)) {
+          if (!version.match(version0))
+            add(`https://${path}/tree/${version}`, githubSelector);
+          add(`https://${path}`, githubSelector);
+        }
+
+        let success = false;
+        const errors: any[] = [];
+        for (const url of urls) {
+          const selector = urlToSelector[url];
+          await page.goto(url);
+          await page.waitForLoadState();
+
+          // await page.pause();
+          try {
+            const lisence = (
+              await page.$eval(selector, el => el.textContent)
+            )?.trim();
+            process.stdout.write(JSON.stringify({path, version, lisence, url}));
+            process.stdout.write('\n');
+            success = true;
+            break;
+          } catch (err: any) {
+            errors.push(err);
+          }
+        }
+        if (!success && errors.length > 0) {
+          process.stderr.write(
+            `failed to get license at ${urls.join(',')} because of ${errors}\n`
+          );
+        }
+      }
+    } finally {
+      await browser.close();
+    }
+  })();
+});
